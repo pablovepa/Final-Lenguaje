28db3e17072ea6c5dc70fdc492e077c8bfce1db7
Pablo Vega <pablovega6543@gmail.com>
2025-12-03 20:35:05 -0300

    .

--- crud_tipo/includes/headerindexu.php
+++ crud_tipo/includes/headerindexu.php
@@ -20,7 +20,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="indexu.php">Inicio <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="indexu.php">Inicio Vinos <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="Usuarios/createu.php">Crear</a>
