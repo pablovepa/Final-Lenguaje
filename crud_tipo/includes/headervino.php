baedc2fae12d241ce1f4a447b707386fb40ed614
leonelmu <leosebmu@gmail.com>
2025-12-08 18:09:53 -0300

    agrego favicon a crud

--- crud_tipo/includes/headervino.php
+++ crud_tipo/includes/headervino.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Vinos</title>
+    <link rel="icon" type="image/png" href="../gamepage/img/favicon.png">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
